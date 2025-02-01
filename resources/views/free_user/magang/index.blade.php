5eb6f768f016c2baccdd1d3c462cff06c74d653b
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-02-02 01:51:43 +0800

    format code

--- resources/views/free_user/magang/index.blade.php
+++ resources/views/free_user/magang/index.blade.php
@@ -2,20 +2,41 @@
 
 @section('content')
   <div class="container d-flex justify-content-center align-items-center vh-80">
-    <div class="card shadow-lg w-100" style="max-width: 900px;">
+    <div
+      class="card shadow-lg w-100"
+      style="max-width: 900px;"
+    >
       <div class="card-body text-left">
         <h3 class="text-primary mb-3 fw-bold">DAFTAR MAGANG</h3>
-        <form id="multiStepForm" action="{{ route('magang.store') }}" method="POST" enctype="multipart/form-data">
+        <form
+          id="multiStepForm"
+          action="{{ route('magang.store') }}"
+          method="POST"
+          enctype="multipart/form-data"
+        >
           @csrf
-          <div class="step" id="step1">
+          <div
+            class="step"
+            id="step1"
+          >
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="name" class="form-label">Nama</label>
+                <label
+                  for="name"
+                  class="form-label"
+                >Nama</label>
               </div>
               <div class="col-sm-9">
-                <input type="text" id="name" name="name"
-                       class="form-control @error('name') is-invalid @enderror" autocomplete="off" autofocus
-                       placeholder="Masukkan Nama" value="{{ old('name') }}" />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  class="form-control @error('name') is-invalid @enderror"
+                  autocomplete="off"
+                  autofocus
+                  placeholder="Masukkan Nama"
+                  value="{{ old('name') }}"
+                />
                 @error('name')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -24,35 +45,62 @@
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="gender" class="form-label">Jenis Kelamin</label>
+                <label
+                  for="gender"
+                  class="form-label"
+                >Jenis Kelamin</label>
               </div>
               <div class="col-sm-4">
-                <input type="radio" id="male" name="gender" value="male"
-                       class="form-check-input @error('gender') is-invalid @enderror"
-                       {{ old('gender') == 'male' ? 'checked' : '' }} />
-                <label for="male" class="form-check-label">Laki-laki</label> <br>
+                <input
+                  type="radio"
+                  id="male"
+                  name="gender"
+                  value="male"
+                  class="form-check-input @error('gender') is-invalid @enderror"
+                  {{ old('gender') == 'male' ? 'checked' : '' }}
+                />
+                <label
+                  for="male"
+                  class="form-check-label"
+                >Laki-laki</label> <br>
                 @error('gender')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
               </div>
               <div class="col-sm-4">
-                <input type="radio" id="female" name="gender" value="female"
-                       class="form-check-input @error('gender') is-invalid @enderror"
-                       {{ old('gender') == 'female' ? 'checked' : '' }}>
-                <label for="female" class="form-check-label">Perempuan</label>
+                <input
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value="female"
+                  class="form-check-input @error('gender') is-invalid @enderror"
+                  {{ old('gender') == 'female' ? 'checked' : '' }}
+                />
+                <label
+                  for="female"
+                  class="form-check-label"
+                >Perempuan</label>
               </div>
             </div>
 
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="institusi" class="form-label">Asal Sekolah/institusi</label>
+                <label
+                  for="institusi"
+                  class="form-label"
+                >Asal Sekolah/institusi</label>
               </div>
               <div class="col-sm-9">
-                <input type="text" id="institusi"
-                       class="form-control @error('institution') is-invalid
-								@enderror" name="institution"
-                       value="{{ old('institution') }}" autocomplete="off" placeholder="Masukkan Asal Sekolah" />
+                <input
+                  type="text"
+                  id="institusi"
+                  class="form-control @error('institution') is-invalid @enderror"
+                  name="institution"
+                  value="{{ old('institution') }}"
+                  autocomplete="off"
+                  placeholder="Masukkan Asal Sekolah"
+                />
 
                 @error('institution')
                   <small class="text-danger">{{ $message }}</small>
@@ -61,22 +109,36 @@
             </div>
 
             <div class="row mb-3">
-              <label class="col-sm-3 col-form-label" for="birthplace">TTL</label>
+              <label
+                class="col-sm-3 col-form-label"
+                for="birthplace"
+              >TTL</label>
               <div class="col-sm-4">
-                <input type="text" class="form-control @error('birthplace') is-invalid
-								@enderror"
-                       value="{{ old('birthplace') }}" id="birthplace" name="birthplace" placeholder="Tempat Lahir"
-                       autocomplete="off" onfocus="this.showPicker()">
+                <input
+                  type="text"
+                  class="form-control @error('birthplace') is-invalid	@enderror"
+                  value="{{ old('birthplace') }}"
+                  id="birthplace"
+                  name="birthplace"
+                  placeholder="Tempat Lahir"
+                  autocomplete="off"
+                  onfocus="this.showPicker()"
+                >
                 <small class="text-primary">Tempat Lahir</small><br>
                 @error('birthplace')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
               </div>
               <div class="col-sm-4">
-                <input type="date" class="form-control @error('date_of_birth') is-invalid
-								@enderror"
-                       value="{{ old('date_of_birth') }}" id="date_of_birth" name="date_of_birth" autocomplete="off"
-                       onfocus="this.showPicker()">
+                <input
+                  type="date"
+                  class="form-control @error('date_of_birth') is-invalid @enderror"
+                  value="{{ old('date_of_birth') }}"
+                  id="date_of_birth"
+                  name="date_of_birth"
+                  autocomplete="off"
+                  onfocus="this.showPicker()"
+                >
                 <small class="text-primary ">Tanggal Lahir</small><br>
                 @error('date_of_birth')
                   <small class="text-danger">{{ $message }}</small>
@@ -85,19 +147,35 @@
             </div>
 
             <div class="row mb-3">
-              <label class="col-sm-3 col-form-label" for="start">Durasi Magang </label>
+              <label
+                class="col-sm-3 col-form-label"
+                for="start"
+              >Durasi Magang </label>
               <div class="col-sm-4">
-                <input type="date" class="form-control @error('start') is-invalid
-									@enderror" id="start"
-                       value="{{ old('start') }}" name="start" autocomplete="off" onfocus="this.showPicker()">
+                <input
+                  type="date"
+                  class="form-control @error('start') is-invalid @enderror"
+                  id="start"
+                  value="{{ old('start') }}"
+                  name="start"
+                  autocomplete="off"
+                  onfocus="this.showPicker()"
+                />
                 <small class="text-primary">Mulai Magang</small><br>
                 @error('start')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
               </div>
               <div class="col-sm-4">
-                <input type="date" class="form-control @error('end') is-invalid @enderror" id="end"
-                       name="end" value="{{ old('end') }}" autocomplete="off" onfocus="this.showPicker()">
+                <input
+                  type="date"
+                  class="form-control @error('end') is-invalid @enderror"
+                  id="end"
+                  name="end"
+                  value="{{ old('end') }}"
+                  autocomplete="off"
+                  onfocus="this.showPicker()"
+                />
                 <small class="text-primary">Selesai Magang</small><br>
                 @error('end')
                   <small class="text-danger">{{ $message }}</small>
@@ -107,14 +185,22 @@
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="phone_number" class="form-label">Nomor HP</label>
+                <label
+                  for="phone_number"
+                  class="form-label"
+                >Nomor HP</label>
               </div>
               <div class="col-sm-9">
-                <input type="text" inputmode="numeric" id="phone_number"
-                       class="form-control @error('phone_number') is-invalid @enderror"
-                       autocomplete="off" name="phone_number" value="{{ old('phone_number') }}"
-                       placeholder="Masukkan Nomor HP" />
-
+                <input
+                  type="text"
+                  inputmode="numeric"
+                  id="phone_number"
+                  class="form-control @error('phone_number') is-invalid @enderror"
+                  autocomplete="off"
+                  name="phone_number"
+                  value="{{ old('phone_number') }}"
+                  placeholder="Masukkan Nomor HP"
+                />
 
                 @error('phone_number')
                   <small class="text-danger">{{ $message }}</small>
@@ -124,12 +210,21 @@
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="email" class="form-label">Email</label>
+                <label
+                  for="email"
+                  class="form-label"
+                >Email</label>
               </div>
               <div class="col-sm-9">
-                <input type="email" id="email" name="email"
-                       class="form-control @error('email') is-invalid @enderror" autocomplete="off"
-                       placeholder="Masukkan Email" value="{{ old('email') }}" />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="form-control @error('email') is-invalid @enderror"
+                  autocomplete="off"
+                  placeholder="Masukkan Email"
+                  value="{{ old('email') }}"
+                />
                 @error('email')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -138,14 +233,22 @@
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="address" class="form-label">Alamat Rumah</label>
+                <label
+                  for="address"
+                  class="form-label"
+                >Alamat Rumah</label>
               </div>
               <div class="col-sm-9">
-                <input type="text" id="address" name="address"
-                       class="form-control @error('address') is-invalid
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  class="form-control @error('address') is-invalid
 									@enderror"
-                       placeholder="Masukkan Alamat"
-                       value="{{ old('address') }}" autocomplete="off" />
+                  placeholder="Masukkan Alamat"
+                  value="{{ old('address') }}"
+                  autocomplete="off"
+                />
                 @error('address')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -154,14 +257,23 @@
 
             <div class="row mb-3">
               <div class="col-sm-3">
-                <label for="parent_number" class="form-label">Nomor HP Orang Tua</label>
+                <label
+                  for="parent_number"
+                  class="form-label"
+                >Nomor HP Orang Tua</label>
               </div>
               <div class="col-sm-9">
-                <input type="text" inputmode="numeric" id="parent_number"
-                       class="form-control @error('parent_number') is-invalid
-									@enderror" autocomplete="off"
-                       name="parent_number" value="{{ old('parent_number') }}"
-                       placeholder="Masukkan Nomor HP Orang Tua" />
+                <input
+                  type="text"
+                  inputmode="numeric"
+                  id="parent_number"
+                  class="form-control @error('parent_number') is-invalid
+									@enderror"
+                  autocomplete="off"
+                  name="parent_number"
+                  value="{{ old('parent_number') }}"
+                  placeholder="Masukkan Nomor HP Orang Tua"
+                />
                 @error('parent_number')
                   <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -170,28 +282,53 @@
 
             <div class="row">
               <div class="col-6">
-                <a href="/" class="btn btn-danger mt-2 w-100">Kembali</a>
+                <a
+                  href="/"
+                  class="btn btn-danger mt-2 w-100"
+                >Kembali</a>
               </div>
               <div class="col-6">
-                <button type="button" class="btn btn-primary w-100 mt-2" onclick="nextStep()">Lanjut</button>
+                <button
+                  type="button"
+                  class="btn btn-primary w-100 mt-2"
+                  onclick="nextStep()"
+                >Lanjut</button>
               </div>
             </div>
           </div>
 
-          <div class="step d-none" id="step2">
+          <div
+            class="step d-none"
+            id="step2"
+          >
             <h4 class="fw-bold">Ambil Foto</h4>
             <div class="row">
               <div class="col-6">
                 <h5 class="fw-bold">Kamera</h5>
                 <div class="border rounded p-2">
-                  <video id="webcam" class="w-100" autoplay></video>
+                  <video
+                    id="webcam"
+                    class="w-100"
+                    autoplay
+                  ></video>
                 </div>
-                <button type="button" class="btn btn-warning w-100 mt-2" onclick="takeSnapshot()">Ambil Foto</button>
+                <button
+                  type="button"
+                  class="btn btn-warning w-100 mt-2"
+                  onclick="takeSnapshot()"
+                >Ambil Foto</button>
               </div>
               <div class="col-6">
                 <h5 class="fw-bold">Foto yang Diambil</h5>
-                <canvas id="snapshot" class="d-none mt-2 w-100"></canvas>
-                <input type="hidden" name="photo" id="imageInput">
+                <canvas
+                  id="snapshot"
+                  class="d-none mt-2 w-100"
+                ></canvas>
+                <input
+                  type="hidden"
+                  name="photo"
+                  id="imageInput"
+                >
               </div>
             </div>
 
@@ -201,11 +338,18 @@
 
             <div class="row">
               <div class="col-6">
-                <button type="button" class="btn btn-secondary w-100 mt-3" onclick="prevStep()">Kembali</button>
+                <button
+                  type="button"
+                  class="btn btn-secondary w-100 mt-3"
+                  onclick="prevStep()"
+                >Kembali</button>
               </div>
 
               <div class="col-6">
-                <button type="submit" class="btn btn-success w-100 mt-3">Selesai</button>
+                <button
+                  type="submit"
+                  class="btn btn-success w-100 mt-3"
+                >Selesai</button>
               </div>
             </div>
           </div>
