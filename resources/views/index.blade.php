2bb03152cc54261eccc03eb94b22ebd15624987c
Muhammad Ichsan Fadillah <muhammad.ichsan.fadillah@mhs.politala.ac.id>
2025-02-01 20:59:42 +0800

    Landing page added & minor change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -92,7 +92,7 @@
       <img src="{{ asset('Dewi/assets/img/hero-bg.jpg')}}" alt="" data-aos="fade-in">
 
       <div class="container d-flex flex-column align-items-center">
-        <h2 data-aos="fade-up" data-aos-delay="100">SELAMAT DATANG WKWK :D</h2>
+        <h2 data-aos="fade-up" data-aos-delay="100">SELAMAT DATANG KNTL :D</h2>
         <p data-aos="fade-up" data-aos-delay="200">We are team of talented designers making websites with Bootstrap</p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
           <a href="#about" class="btn-get-started">Narasumber</a>
