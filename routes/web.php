3e6dfd7b9ce6fa5659fed242b9a9a4c1085c31e9
Muhammad Ichsan Fadillah <muhammad.ichsan.fadillah@mhs.politala.ac.id>
2025-02-01 09:17:15 +0800

    Landing Page template added

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-	return view('welcome');
+	return view('user.index');
 });
 
 // Route autentikasi
